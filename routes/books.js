05b92036c1143c53a646d5bb3ae9e8250d9fb412
bunyamin fil <bunyaminfl@gmail.com>
2020-11-07 04:58:01 -0500

    get and put api

--- routes/books.js
+++ routes/books.js
@@ -26,4 +26,47 @@ router.post("/", function (req, res, next) {
   });
 });
 
+//get all data
+router.get("/all", (req, res) => {
+  Book.find({}, (err, data) => {
+    res.json(data);
+  });
+});
+
+router.get("/search", (req, res) => {
+  Book.find({ published: false }, (err, data) => {
+    res.json(data);
+  });
+});
+
+router.get("/searchOne", (req, res) => {
+  Book.findOne({ title: "Küçük Prens" }, (err, data) => {
+    res.json(data);
+  });
+});
+
+router.get("/searchById", (req, res) => {
+  Book.findById("5fa56063315cce912c431eab", (err, data) => {
+    res.json(data);
+  });
+});
+//Book.update ilk bulduğu kaydı günceller
+router.put("/update", (req, res) => {
+  Book.update({ published: false }, { published: true }, (err, data) => {
+    res.json(data);
+  });
+});
+
+//birden fazla alanda güncelleme için multi keyword kullanılması gerekli
+router.put("/updateall", (req, res) => {
+  Book.update(
+    { published: false },
+    { published: true },
+    { multi: true },
+    (err, data) => {
+      res.json(data);
+    }
+  );
+});
+
 module.exports = router;
